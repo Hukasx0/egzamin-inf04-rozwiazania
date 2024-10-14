29ce7ca03c32d0785e759ca2e14b1deeb27a8cdd
Hubert Kasperek <hubertkasp13@gmail.com>
2024-10-14 13:28:27 +0200

    Dodaj aplikacje konsolowa z czerwca 2023

---
+++ czerwiec-2023/README.md
@@ -0,0 +1,3 @@
+# Arkusz czerwiec 2023
+
+[Link do arkusza](https://arkusze.pl/zawodowy/inf04-2023-czerwiec-egzamin-zawodowy-praktyczny.pdf)
---
+++ czerwiec-2023/aplikacja-konsolowa/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace aplikacja_konsolowa
+{
+    internal class Program
+    {
+        static void SieveOfEratosthenes(bool[] primes, int n)
+        {
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (primes[i])
+                {
+                    for (int j = i * i; j <= n; j += i)
+                    {
+                        primes[j] = false;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 100;
+            bool[] primes = new bool[n + 1];
+
+            // Inicjalizacja tablicy: wszystkie liczby są domyślnie uznawane za pierwsze
+            for (int i = 2; i <= n; i++)
+            {
+                primes[i] = true;
+            }
+
+            // Wypełnienie tablicy zgodnie z algorytmem sita Eratostenesa
+            SieveOfEratosthenes(primes, n);
+
+            // Wyświetlenie liczb pierwszych
+            Console.WriteLine("Liczby pierwsze w przedziale 2..100:");
+            for (int i = 2; i <= n; i++)
+            {
+                if (primes[i])
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
