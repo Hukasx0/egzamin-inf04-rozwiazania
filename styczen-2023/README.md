fdf128fc1ed9e3669126f8a44377e59da7e7ea61
Hubert Kasperek <hubertkasp13@gmail.com>
2024-10-14 12:51:54 +0200

    Dodaj aplikacje konsolowa ze stycznia 2023

---
+++ styczen-2023/README.md
@@ -0,0 +1,3 @@
+# Arkusz styczeń 2023
+
+[Link do arkusza](https://arkusze.pl/zawodowy/inf04-2023-styczen-egzamin-zawodowy-praktyczny.pdf)
---
+++ styczen-2023/aplikacja-konsolowa/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace aplikacja_konsolowa
+{
+    internal class Program
+    {
+        class Euklides
+        {
+            // Funkcja obliczająca NWD za pomocą algorytmu Euklidesa
+            public static int ObliczNwd(int a, int b)
+            {
+                while (b != 0)
+                {
+                    int temp = b;
+                    b = a % b;
+                    a = temp;
+                }
+                return a;
+            }
+
+            static void Main(string[] args)
+            {
+                int a, b;
+
+                // Wczytywanie liczb a i b od użytkownika
+                Console.Write("Wprowadź pierwszą liczbę całkowitą dodatnią (a): ");
+                while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+                {
+                    Console.Write("Błąd! Proszę wprowadzić liczbę całkowitą dodatnią: ");
+                }
+
+                Console.Write("Wprowadź drugą liczbę całkowitą dodatnią (b): ");
+                while (!int.TryParse(Console.ReadLine(), out b) || b <= 0)
+                {
+                    Console.Write("Błąd! Proszę wprowadzić liczbę całkowitą dodatnią: ");
+                }
+
+                // Obliczanie NWD
+                int nwd = ObliczNwd(a, b);
+
+                // Wyświetlanie wyniku
+                Console.WriteLine($"Największy wspólny dzielnik ({a}, {b}) to: {nwd}");
+            }
+        }
+    }
+}
