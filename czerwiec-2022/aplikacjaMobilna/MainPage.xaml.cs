bcbf2440801e122a44aaca41f3670387953d72c2
Hubert Kasperek <hubertkasp13@gmail.com>
2024-11-22 19:01:56 +0100

    Dodaj aplikacje mobilną z czerwca 2022

---
+++ czerwiec-2022/aplikacjaMobilna/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace aplikacjaMobilna
+{
+    public partial class MainPage : ContentPage
+    {
+        // Zmienna do przechowywania liczby polubień
+        private int likesCount = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Funkcja obsługująca przycisk "POLUB"
+        // inkrementujemy polubienia
+        private void LikeButton_Clicked(object sender, EventArgs e)
+        {
+            likesCount++;
+            UpdateLikesText();
+        }
+
+        // Funkcja obsługująca przycisk "USUŃ"
+        // dekrementujemy polubienia
+        private void UnlikeButton_Clicked(object sender, EventArgs e)
+        {
+            if (likesCount > 0)
+            {
+                likesCount--;
+            }
+            UpdateLikesText();
+        }
+
+        // Funkcja obsługująca przycisk "ZAPISZ" (bez logiki, w treści zadania ten przycisk nic nie miał robić)
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            // Opcjonalne, nie ma tego w treści zadania
+            DisplayAlert("Zapisano", "Kliknięto przycisk \"ZAPISZ\"", "OK");
+        }
+
+        // Funkcja aktualizująca tekst, który pokazuje liczbę polubień
+        private void UpdateLikesText()
+        {
+            likesText.Text = $"{likesCount} polubień";
+        }
+    }
+
+}
